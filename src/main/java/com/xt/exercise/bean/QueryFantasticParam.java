473ed31cbf254529b6b7e749ee8e435de60ff79d
tao.xiong <tao.xiong@11bee.com>
2020-01-14 17:29:04 +0800

    valid

--- src/main/java/com/xt/exercise/bean/QueryFantasticParam.java
+++ src/main/java/com/xt/exercise/bean/QueryFantasticParam.java
@@ -1,9 +1,26 @@
 package com.xt.exercise.bean;
 
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.NotBlank;
+
 /**
  * @Author: tao.xiong
  * @Date: 2019/12/25 17:22
  * @Description:
  */
-public final class QueryFantasticParam {
+public final class QueryFantasticParam implements Serializable {
+
+    private static final long serialVersionUID = 6983823845626912068L;
+
+    @NotBlank(message = "业务线Id不能为空")
+    private String BusinessId;
+
+    public String getBusinessId() {
+        return BusinessId;
+    }
+
+    public void setBusinessId(String businessId) {
+        BusinessId = businessId;
+    }
 }
